488eb5c667c383b20e639483917a925d29510264
vasilsmacbook <vaseto.k28@gmail.com>
2023-07-25 13:56:24 +0300

    Updated DefaultApplicationUser

---
+++ VAndDCargoes/VAndDCargoes.Data.Models/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace VAndDCargoes.Data.Models;
+
+public class ApplicationUser : IdentityUser<Guid>
+{
+    public ApplicationUser()
+    {
+        this.Id = Guid.NewGuid();
+    }
+}
+

d2238db7bee17447972a5615798125623befdd5a
vasilsmacbook <vaseto.k28@gmail.com>
2023-07-25 13:56:10 +0300

    Created Cargo entity model

---
+++ VAndDCargoes/VAndDCargoes.Data.Models/Cargo.cs
@@ -0,0 +1,36 @@
+﻿using static VAndDCargoes.Common.EntitiesValidations.Cargo;
+using System.ComponentModel.DataAnnotations;
+using VAndDCargoes.Data.Models.Enumerations;
+
+namespace VAndDCargoes.Data.Models;
+
+public class Cargo
+{
+    public Cargo()
+    {
+        this.Id = Guid.NewGuid();
+        this.Trailers = new HashSet<Trailer>();
+    }
+
+    [Key]
+    public Guid Id { get; set; }
+
+    [Required]
+    [MaxLength(NameMaxLength)]
+    public string Name { get; set; } = null!;
+
+    [Required]
+    [MaxLength(DescriptionMaxLength)]
+    public string Description { get; set; } = null!;
+
+    public int Weight { get; set; }
+
+    [Required]
+    public CargoCategory Category { get; set; }
+
+    [Required]
+    public CargoPhysicalState PhysicalState { get; set; }
+
+    public virtual ICollection<Trailer> Trailers { get; set; }
+}
+

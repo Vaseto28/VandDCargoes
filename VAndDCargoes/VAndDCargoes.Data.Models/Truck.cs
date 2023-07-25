7633f84ab7a8b993a9b66cc5edfee262affc93f9
vasilsmacbook <vaseto.k28@gmail.com>
2023-07-25 13:55:33 +0300

    Created Truck entity model

---
+++ VAndDCargoes/VAndDCargoes.Data.Models/Truck.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.DataAnnotations;
+using VAndDCargoes.Data.Models.Enumerations;
+using static VAndDCargoes.Common.EntitiesValidations.Truck;
+
+namespace VAndDCargoes.Data.Models;
+
+public class Truck
+{
+    public Truck()
+    {
+        this.Id = Guid.NewGuid();
+    }
+
+    [Key]
+    public Guid Id { get; set; }
+
+    [Required]
+    [MaxLength(MakeMaxLength)]
+    public string Make { get; set; } = null!;
+
+    [Required]
+    [MaxLength(ModelMaxLength)]
+    public string Model { get; set; } = null!;
+
+    [Required]
+    [MaxLength(RegistrationNumberMaxLength)]
+    public string RegistrationNumber { get; set; } = null!;
+
+    [Required]
+    public TruckCondition Condition { get; set; }
+
+    public int TraveledDistance { get; set; }
+
+    public int FuelCapacity { get; set; }
+
+    public Guid? DriverId { get; set; }
+
+    public virtual Driver? Driver { get; set; }
+
+    public Guid? TraillerId { get; set; }
+
+    public virtual Trailer? Trailer { get; set; }
+}
+

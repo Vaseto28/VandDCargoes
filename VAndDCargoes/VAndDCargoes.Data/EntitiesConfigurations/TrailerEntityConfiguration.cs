d350d6ac126b2db10e6c82aa9962d684bf2f967e
vasilsmacbook <vaseto.k28@gmail.com>
2023-07-26 11:14:44 +0300

    Created TrailerEntityConfiguration

---
+++ VAndDCargoes/VAndDCargoes.Data/EntitiesConfigurations/TrailerEntityConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using VAndDCargoes.Data.Models;
+
+namespace VAndDCargoes.Data.EntitiesConfigurations;
+
+public class TrailerEntityConfiguration : IEntityTypeConfiguration<Trailer>
+{
+    public void Configure(EntityTypeBuilder<Trailer> builder)
+    {
+        builder
+            .HasOne(x => x.Cargo)
+            .WithMany(x => x.Trailers)
+            .HasForeignKey(x => x.CargoId)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
+}
+

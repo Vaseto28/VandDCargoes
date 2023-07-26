22e5e617c074863ae438634bd2f16475606fd949
vasilsmacbook <vaseto.k28@gmail.com>
2023-07-26 11:17:06 +0300

    Created a slider

---
+++ VAndDCargoes/VAndDCargoes.Web/wwwroot/js/sliderScript.js
@@ -0,0 +1,37 @@
+﻿$('.responsive').slick({
+    dots: true,
+    prevArrow: $('.prev'),
+    nextArrow: $('.next'),
+    infinite: false,
+    speed: 100,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    responsive: [
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3
+            }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+    ]
+});

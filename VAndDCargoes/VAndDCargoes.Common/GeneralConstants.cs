cecb85039292573cfab764952a581948af6cb6a3
vasilsmacbook <vaseto.k28@gmail.com>
2023-07-26 11:14:10 +0300

    Added Common class library to hold constants and constraints for the application

--- VAndDCargoes/VAndDCargoes.Common/GeneralConstants.cs
+++ VAndDCargoes/VAndDCargoes.Common/GeneralConstants.cs
@@ -2,6 +2,9 @@
 
 public static class GeneralConstants
 {
-	public const int ReleaseYear = 2023;	
+	public const int ReleaseYear = 2023;
+
+	public const int DefaultPage = 1;
+	public const int DefaultTrucksPerPage = 5;
 }
 

95a6aaa0cc7f18070cfd0f37e0cc218e33c6cbf8
vasilsmacbook <vaseto.k28@gmail.com>
2023-07-26 11:14:29 +0300

    Created ApplicationDbContext

---
+++ VAndDCargoes/VAndDCargoes.Data/ApplicationDbContext.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using VAndDCargoes.Data.Models;
+
+namespace VAndDCargoes.Data;
+
+public class VAndDCargoesDbContext : IdentityDbContext<ApplicationUser, IdentityRole<Guid>, Guid>
+{
+    public VAndDCargoesDbContext(DbContextOptions<VAndDCargoesDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Truck> Trucks { get; set; } = null!;
+
+    public DbSet<Trailer> Trailers { get; set; } = null!;
+
+    public DbSet<Cargo> Cargoes { get; set; } = null!;
+
+    public DbSet<Driver> Drivers { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        Assembly assembly = Assembly.GetAssembly(typeof(VAndDCargoesDbContext)) ?? Assembly.GetExecutingAssembly();
+
+        builder.ApplyConfigurationsFromAssembly(assembly);
+
+        base.OnModelCreating(builder);
+    }
+}
+

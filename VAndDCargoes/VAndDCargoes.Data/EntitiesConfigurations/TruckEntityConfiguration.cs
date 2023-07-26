165a006507243273ccd5fc69c3c0fe1cf70854f0
vasilsmacbook <vaseto.k28@gmail.com>
2023-07-26 11:14:56 +0300

    Created TruckEntityConfiguration

---
+++ VAndDCargoes/VAndDCargoes.Data/EntitiesConfigurations/TruckEntityConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using VAndDCargoes.Data.Models;
+
+namespace VAndDCargoes.Data.EntitiesConfigurations;
+
+public class TruckEntityConfiguration : IEntityTypeConfiguration<Truck>
+{
+    public void Configure(EntityTypeBuilder<Truck> builder)
+    {
+        builder
+            .HasOne(x => x.Driver)
+            .WithMany(x => x.Trucks)
+            .HasForeignKey(x => x.DriverId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder
+            .HasOne(x => x.Trailer)
+            .WithMany(x => x.Trucks)
+            .HasForeignKey(x => x.TraillerId)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
+}
+

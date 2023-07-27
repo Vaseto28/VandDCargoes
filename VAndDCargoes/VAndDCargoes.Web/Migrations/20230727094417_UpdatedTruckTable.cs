eb22cedf183e7161ded6e198eef2d555be09c876
vasilsmacbook <vaseto.k28@gmail.com>
2023-07-27 12:58:07 +0300

    Updated Trucks table

---
+++ VAndDCargoes/VAndDCargoes.Web/Migrations/20230727094417_UpdatedTruckTable.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VAndDCargoes.Web.Migrations
+{
+    public partial class UpdatedTruckTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedOn",
+                table: "Trucks",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedOn",
+                table: "Trucks");
+        }
+    }
+}

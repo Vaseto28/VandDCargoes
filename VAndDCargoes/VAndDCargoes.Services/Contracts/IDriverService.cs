0fe8587039a437dc3fd59d565417b96b1392462a
vasilsmacbook <vaseto.k28@gmail.com>
2023-07-28 11:44:30 +0300

    Created DriverService with implementation

---
+++ VAndDCargoes/VAndDCargoes.Services/Contracts/IDriverService.cs
@@ -0,0 +1,11 @@
+﻿using VAndDCargoes.Web.ViewModels.Driver;
+
+namespace VAndDCargoes.Services.Contracts;
+
+public interface IDriverService
+{
+    Task<bool> IsTheUserAlreadyDriver(string id);
+
+    Task BecomeDriverAsync(BecomeDriverViewModel model, string userId);
+}
+
---
+++ VAndDCargoes/VAndDCargoes.Services/DriverService.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VAndDCargoes.Data;
+using VAndDCargoes.Data.Models;
+using VAndDCargoes.Services.Contracts;
+using VAndDCargoes.Web.ViewModels.Driver;
+
+namespace VAndDCargoes.Services;
+
+public class DriverService : IDriverService
+{
+    private readonly VAndDCargoesDbContext dbContext;
+
+    public DriverService(VAndDCargoesDbContext dbContext)
+    {
+        this.dbContext = dbContext;
+    }
+
+    public async Task BecomeDriverAsync(BecomeDriverViewModel model, string userId)
+    {
+        Driver driver = new Driver()
+        {
+            FirstName = model.FirstName,
+            SecondName = model.SecondName,
+            LastName = model.LastName,
+            PhoneNumber = model.PhoneNumber,
+            Age = model.Age,
+            UserId = Guid.Parse(userId)
+        };
+
+        await this.dbContext.Drivers.AddAsync(driver);
+        await this.dbContext.SaveChangesAsync();
+    }
+
+    public async Task<bool> IsTheUserAlreadyDriver(string id)
+    {
+        if (await this.dbContext.Drivers.AnyAsync(x => x.UserId.ToString().Equals(id)))
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
+

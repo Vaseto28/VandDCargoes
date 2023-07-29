145f372bc589c7e50ca91d4cfcc6fe40a5dc1da0
vasilsmacbook <vaseto.k28@gmail.com>
2023-07-29 10:03:56 +0300

    Updated ApplicationUser entity

--- VAndDCargoes/VAndDCargoes.Data.Models/ApplicationUser.cs
+++ VAndDCargoes/VAndDCargoes.Data.Models/ApplicationUser.cs
@@ -9,10 +9,16 @@ public class ApplicationUser : IdentityUser<Guid>
         this.Id = Guid.NewGuid();
         this.Drivers = new HashSet<Driver>();
         this.Trucks = new HashSet<Truck>();
+        this.Trailers = new HashSet<Trailer>();
+        this.Cargoes = new HashSet<Cargo>();
     }
 
     public virtual ICollection<Driver> Drivers { get; set; }
 
     public virtual ICollection<Truck> Trucks { get; set; }
+
+    public virtual ICollection<Trailer> Trailers { get; set; }
+
+    public virtual ICollection<Cargo> Cargoes { get; set; }
 }
 

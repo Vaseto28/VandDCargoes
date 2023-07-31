4649082eef447d4e70115cb45f1e0fce6ab1ca49
vasilsmacbook <vaseto.k28@gmail.com>
2023-07-31 16:57:06 +0300

    Created some extensions

---
+++ VAndDCargoes/VAndDCargoes.Web.Infrastructure/Extentions/WebApplicationBuilderExtentions.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace VAndDCargoes.Web.Infrastructure.Extentions;
+
+/// <summary>
+/// Adding services automatically via reflection.
+/// </summary>
+
+public static class WebApplicationBuilderExtentions
+{
+    public static void AddApplicationServices(this IServiceCollection services, Type serviceType)
+	{
+		Assembly? serviceAssembly = Assembly.GetAssembly(serviceType);
+		if (serviceAssembly == null)
+		{
+			throw new InvalidOperationException("Invalid service type provided!");
+		}
+
+		IEnumerable<Type> serviceTypes = serviceAssembly
+			.GetTypes()
+			.Where(x => x.Name.EndsWith("Service") && !x.IsInterface);
+
+		foreach (var implementationType in serviceTypes)
+		{
+			Type? interfaceType = implementationType.GetInterface($"I{implementationType.Name}");
+			if (interfaceType == null)
+			{
+				throw new InvalidOperationException("Invalid service contract type provided!");
+			}
+
+			services.AddScoped(interfaceType, implementationType);
+		}
+	}
+}
+
+ 

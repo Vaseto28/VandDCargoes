4e61fa3be49c331c0afe2f6fd6b31afb5b53fc49
vasilsmacbook <vaseto.k28@gmail.com>
2023-07-31 16:57:41 +0300

    Created CargoAllViewModel

---
+++ VAndDCargoes/VAndDCargoes.Web.Models/Cargo/CargoAllViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static VAndDCargoes.Common.EntitiesValidations.Cargo;
+
+namespace VAndDCargoes.Web.ViewModels.Cargo;
+
+public class CargoAllViewModel
+{
+	[Required]
+	public string Id { get; set; } = null!;
+
+	[Required]
+	[StringLength(NameMaxLength, MinimumLength = NameMinLength)]
+	public string Name { get; set; } = null!;
+
+	[Required]
+	[StringLength(DescriptionMaxLength, MinimumLength = DescriptionMinLength)]
+	public string Description { get; set; } = null!;
+
+	[Range(WeightMinLength, WeightMaxLength)]
+	public int Weight { get; set; }
+
+	[Required]
+	public string Category { get; set; } = null!;
+
+	[Required]
+	public string PhysicalState { get; set; } = null!;
+}
+

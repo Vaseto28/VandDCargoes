caf7067e715812f7c0bc121d382b046a9db81eef
vasilsmacbook <vaseto.k28@gmail.com>
2023-07-31 16:58:42 +0300

    Created BecomeDriverViewModel

---
+++ VAndDCargoes/VAndDCargoes.Web.Models/Driver/BecomeDriverViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace VAndDCargoes.Web.ViewModels.Driver;
+
+public class BecomeDriverViewModel
+{
+	public string FirstName { get; set; } = null!;
+
+	public string SecondName { get; set; } = null!;
+
+	public string LastName { get; set; } = null!;
+
+	public string PhoneNumber { get; set; } = null!;
+
+	public int Age { get; set; }
+}
+

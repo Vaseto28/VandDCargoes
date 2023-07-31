9936753c059f98ffa826b02586201edf330618d7
vasilsmacbook <vaseto.k28@gmail.com>
2023-07-31 16:57:58 +0300

    Created CargoAllViewModel query

---
+++ VAndDCargoes/VAndDCargoes.Web.Models/Cargo/CargoQueryAllViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static VAndDCargoes.Common.GeneralConstants;
+
+namespace VAndDCargoes.Web.ViewModels.Cargo;
+
+public class CargoQueryAllViewModel
+{
+    public CargoQueryAllViewModel()
+    {
+        this.Cargoes = new HashSet<CargoAllViewModel>();
+
+        this.CurrentPage = DefaultPage;
+        this.CargoesPerPage = DefaultCargoesPerPage;
+    }
+
+    public string? Keyword { get; set; }
+
+    public int CurrentPage { get; set; }
+
+    [Display(Name = "Cargoes per page:")]
+    public int CargoesPerPage { get; set; }
+
+    [Display(Name = "Order cargoes by:")]
+    public CargoesOrdering CargoesOrdering { get; set; }
+
+    public virtual IEnumerable<CargoAllViewModel> Cargoes { get; set; }
+}
+

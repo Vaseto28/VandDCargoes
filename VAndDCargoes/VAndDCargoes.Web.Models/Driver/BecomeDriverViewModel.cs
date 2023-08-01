4f641ace9ce9325889118d1c490fc762af402bbe
vasilsmacbook <vaseto.k28@gmail.com>
2023-08-01 11:17:58 +0300

    Created become driver view model

--- VAndDCargoes/VAndDCargoes.Web.Models/Driver/BecomeDriverViewModel.cs
+++ VAndDCargoes/VAndDCargoes.Web.Models/Driver/BecomeDriverViewModel.cs
@@ -1,13 +1,24 @@
-﻿namespace VAndDCargoes.Web.ViewModels.Driver;
+﻿using System.ComponentModel.DataAnnotations;
+using static VAndDCargoes.Common.EntitiesValidations.Driver;
+
+namespace VAndDCargoes.Web.ViewModels.Driver;
 
 public class BecomeDriverViewModel
 {
+	[Required]
+	[StringLength(FirstNameMaxLength, MinimumLength = FirstNameMinLength)]
 	public string FirstName { get; set; } = null!;
 
+	[Required]
+	[StringLength(SecondNameMaxLength, MinimumLength = SecondNameMinLength)]
 	public string SecondName { get; set; } = null!;
 
+	[Required]
+	[StringLength(LastNameMaxLength, MinimumLength = LastNameMinLength)]
 	public string LastName { get; set; } = null!;
 
+	[Phone]
+	[StringLength(PhoneNumberMaxLength, MinimumLength = PhoneNumberMinLength)]
 	public string PhoneNumber { get; set; } = null!;
 
 	public int Age { get; set; }

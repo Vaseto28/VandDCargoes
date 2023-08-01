b3bfb2204a5469eaaaed8c45f6e47e34af58ed6d
vasilsmacbook <vaseto.k28@gmail.com>
2023-08-01 11:19:00 +0300

    Created Base controller with some extension methods

---
+++ VAndDCargoes/VAndDCargoes.Web/Controllers/BaseController.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VAndDCargoes.Web.Controllers;
+
+[Authorize]
+public class BaseController : Controller
+{
+    protected string GetUserId()
+    {
+        return this.User.FindFirstValue(ClaimTypes.NameIdentifier);
+    }
+}
+

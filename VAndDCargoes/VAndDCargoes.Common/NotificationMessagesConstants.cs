02bbb6de97eb4be9d8ba066f9532a9d5f1b7fc2b
vasilsmacbook <vaseto.k28@gmail.com>
2023-08-03 13:43:17 +0300

    Created class to hold notification messages

---
+++ VAndDCargoes/VAndDCargoes.Common/NotificationMessagesConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace VAndDCargoes.Common;
+
+public static class NotificationMessagesConstants
+{
+	public const string SuccessMessage = "Success message!";
+	public const string InformationMessage = "Information message.";
+	public const string WarnMessage = "Warn message!";
+	public const string ErrorMessage = "Error message";
+}
+

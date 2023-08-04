dc19a9ebdccae85c77c8f2768b2298ee240af5b1
vasilsmacbook <vaseto.k28@gmail.com>
2023-08-04 14:16:44 +0300

    Created UserController class for custom identity models and views

---
+++ VAndDCargoes/VAndDCargoes.Web/Controllers/UserController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using VAndDCargoes.Data.Models;
+using VAndDCargoes.Web.ViewModels.User;
+
+namespace VAndDCargoes.Web.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly SignInManager<ApplicationUser> signInManager;
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly IUserStore<ApplicationUser> userStore;
+
+        public UserController(SignInManager<ApplicationUser> signInManager,
+                              UserManager<ApplicationUser> userManager,
+                              IUserStore<ApplicationUser> userStore)
+        {
+            this.signInManager = signInManager;
+            this.userManager = userManager;
+            this.userStore = userStore;
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            ApplicationUser user = new ApplicationUser();
+
+            await this.userManager.SetUserNameAsync(user, model.Username);
+            await this.userManager.SetEmailAsync(user, model.Email);
+
+            IdentityResult result = await this.userManager.CreateAsync(user);
+            if (!result.Succeeded)
+            {
+                foreach (var error in result.Errors)
+                {
+                    this.ModelState.AddModelError("Register error", error.Description);
+                }
+            }
+
+            await this.signInManager.SignInAsync(user, false);
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
+

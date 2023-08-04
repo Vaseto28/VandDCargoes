7a4def008037196c0d84bb46a2631a0d5917af45
vasilsmacbook <vaseto.k28@gmail.com>
2023-08-04 10:36:44 +0300

    Created custom Register view model

---
+++ VAndDCargoes/VAndDCargoes.Web.Models/User/RegisterViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static VAndDCargoes.Common.EntitiesValidations.User;
+
+namespace VAndDCargoes.Web.ViewModels.User;
+
+public class RegisterViewModel
+{
+    [Required]
+    [EmailAddress]
+    [Display(Name = "Email")]
+    public string Email { get; set; } = null!;
+
+    [Required]
+    [StringLength(PasswordMaxLength, MinimumLength = PasswordMinLength, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.")]
+    [DataType(DataType.Password)]
+    [Display(Name = "Password")]
+    public string Password { get; set; } = null!;
+
+    [DataType(DataType.Password)]
+    [Display(Name = "Confirm password")]
+    [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+    public string ConfirmPassword { get; set; } = null!;
+
+    [Required]
+    [Display(Name = "Username")]
+    public string Username { get; set; } = null!;
+}
+

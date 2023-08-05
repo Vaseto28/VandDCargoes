9deeddec085d6a9af5f68b2f48f15a799463f333
vasilsmacbook <vaseto.k28@gmail.com>
2023-08-05 14:13:02 +0300

    Updated BecomeDriver view model

--- VAndDCargoes/VAndDCargoes.Web.Models/Driver/BecomeDriverViewModel.cs
+++ VAndDCargoes/VAndDCargoes.Web.Models/Driver/BecomeDriverViewModel.cs
@@ -6,18 +6,22 @@ namespace VAndDCargoes.Web.ViewModels.Driver;
 public class BecomeDriverViewModel
 {
 	[Required]
+	[Display(Name = "First name")]
 	[StringLength(FirstNameMaxLength, MinimumLength = FirstNameMinLength)]
 	public string FirstName { get; set; } = null!;
 
 	[Required]
+	[Display(Name = "Second name")]
 	[StringLength(SecondNameMaxLength, MinimumLength = SecondNameMinLength)]
 	public string SecondName { get; set; } = null!;
 
 	[Required]
+	[Display(Name = "Last name")]
 	[StringLength(LastNameMaxLength, MinimumLength = LastNameMinLength)]
 	public string LastName { get; set; } = null!;
 
 	[Phone]
+	[Display(Name = "Phone number")]
 	[StringLength(PhoneNumberMaxLength, MinimumLength = PhoneNumberMinLength)]
 	public string PhoneNumber { get; set; } = null!;
 

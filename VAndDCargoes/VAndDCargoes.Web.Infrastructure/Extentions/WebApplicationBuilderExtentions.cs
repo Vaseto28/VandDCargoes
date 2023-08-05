a78312b2d665c57426cc82b76d11effe8fa4f778
vasilsmacbook <vaseto.k28@gmail.com>
2023-08-05 14:12:45 +0300

    Added extension methods to seed Administrator and Specialist roles

--- VAndDCargoes/VAndDCargoes.Web.Infrastructure/Extentions/WebApplicationBuilderExtentions.cs
+++ VAndDCargoes/VAndDCargoes.Web.Infrastructure/Extentions/WebApplicationBuilderExtentions.cs
@@ -1,14 +1,19 @@
 ﻿using System.Reflection;
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.DependencyInjection;
+
+using VAndDCargoes.Data.Models;
+using static VAndDCargoes.Common.GeneralConstants;
 
 namespace VAndDCargoes.Web.Infrastructure.Extentions;
 
-/// <summary>
-/// Adding services automatically via reflection.
-/// </summary>
-
 public static class WebApplicationBuilderExtentions
 {
+    /// <summary>
+    /// Adding services automatically via reflection.
+    /// </summary>
     public static void AddApplicationServices(this IServiceCollection services, Type serviceType)
 	{
 		Assembly? serviceAssembly = Assembly.GetAssembly(serviceType);
@@ -32,6 +37,72 @@ public static class WebApplicationBuilderExtentions
 			services.AddScoped(interfaceType, implementationType);
 		}
 	}
+
+    public static IApplicationBuilder SeedAdministrator(this IApplicationBuilder app, string email)
+    {
+        using IServiceScope scopedServices = app.ApplicationServices.CreateScope();
+
+        IServiceProvider serviceProvider = scopedServices.ServiceProvider;
+
+        UserManager<ApplicationUser> userManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+
+        RoleManager<IdentityRole<Guid>> roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole<Guid>>>();
+
+        Task.Run(async () =>
+        {
+            if (await roleManager.RoleExistsAsync(AdminRoleName))
+            {
+                return;
+            }
+
+            IdentityRole<Guid> role =
+                new IdentityRole<Guid>(AdminRoleName);
+
+            await roleManager.CreateAsync(role);
+
+            ApplicationUser adminUser =
+                await userManager.FindByEmailAsync(email);
+
+            await userManager.AddToRoleAsync(adminUser, AdminRoleName);
+        })
+        .GetAwaiter()
+        .GetResult();
+
+        return app;
+    }
+
+    public static IApplicationBuilder SeedSpecialist(this IApplicationBuilder app, string email)
+    {
+        using IServiceScope scopedServices = app.ApplicationServices.CreateScope();
+
+        IServiceProvider serviceProvider = scopedServices.ServiceProvider;
+
+        UserManager<ApplicationUser> userManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+
+        RoleManager<IdentityRole<Guid>> roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole<Guid>>>();
+
+        Task.Run(async () =>
+        {
+            if (await roleManager.RoleExistsAsync(SpecialistRoleName))
+            {
+                return;
+            }
+
+            IdentityRole<Guid> role =
+                new IdentityRole<Guid>(SpecialistRoleName);
+
+            await roleManager.CreateAsync(role);
+
+            ApplicationUser specialistUser =
+                await userManager.FindByEmailAsync(email);
+
+            await userManager.AddToRoleAsync(specialistUser, SpecialistRoleName);
+        })
+        .GetAwaiter()
+        .GetResult();
+
+        return app;
+    }
 }
 
  

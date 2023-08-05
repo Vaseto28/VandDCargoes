8e3a7cf661f2a64ad17f89f5b3f45ad1c1d5af00
vasilsmacbook <vaseto.k28@gmail.com>
2023-08-05 14:13:18 +0300

    Created Login view mdoel

---
+++ VAndDCargoes/VAndDCargoes.Web.Models/User/LoginViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static VAndDCargoes.Common.EntitiesValidations.User;
+
+namespace VAndDCargoes.Web.ViewModels.User;
+
+public class LoginViewModel
+{
+	[Required]
+	[StringLength(UsernameMaxLength, MinimumLength = UsernameMinLength)]
+	public string Username { get; set; } = null!;
+
+	[Required]
+	[DataType(DataType.Password)]
+	public string Password { get; set; } = null!;
+
+	[Display(Name = "Remember me?")]
+	public bool RememberMe { get; set; }
+
+	public string? ReturnUrl { get; set; }
+}
+

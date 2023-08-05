ae51792d0052c70d4210ce5eaaafb1f130b81082
vasilsmacbook <vaseto.k28@gmail.com>
2023-08-05 14:14:44 +0300

    Updated UserController class

--- VAndDCargoes/VAndDCargoes.Web/Controllers/UserController.cs
+++ VAndDCargoes/VAndDCargoes.Web/Controllers/UserController.cs
@@ -2,6 +2,8 @@
 using Microsoft.AspNetCore.Mvc;
 using VAndDCargoes.Data.Models;
 using VAndDCargoes.Web.ViewModels.User;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.AspNetCore.Authentication;
 
 namespace VAndDCargoes.Web.Controllers
 {
@@ -9,15 +11,15 @@ namespace VAndDCargoes.Web.Controllers
     {
         private readonly SignInManager<ApplicationUser> signInManager;
         private readonly UserManager<ApplicationUser> userManager;
-        private readonly IUserStore<ApplicationUser> userStore;
+        private readonly IMemoryCache memoryCache;
 
         public UserController(SignInManager<ApplicationUser> signInManager,
                               UserManager<ApplicationUser> userManager,
-                              IUserStore<ApplicationUser> userStore)
+                              IMemoryCache memoryCache)
         {
             this.signInManager = signInManager;
             this.userManager = userManager;
-            this.userStore = userStore;
+            this.memoryCache = memoryCache;
         }
 
         [HttpGet]
@@ -39,18 +41,56 @@ namespace VAndDCargoes.Web.Controllers
             await this.userManager.SetUserNameAsync(user, model.Username);
             await this.userManager.SetEmailAsync(user, model.Email);
 
-            IdentityResult result = await this.userManager.CreateAsync(user);
+            IdentityResult result = await this.userManager.CreateAsync(user, model.Password);
             if (!result.Succeeded)
             {
                 foreach (var error in result.Errors)
                 {
-                    this.ModelState.AddModelError("Register error", error.Description);
+                    this.ModelState.AddModelError(string.Empty, error.Description);
                 }
+
+                return View(model);
             }
 
             await this.signInManager.SignInAsync(user, false);
+            this.memoryCache.Remove(memoryCache);
 
             return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Login(string? returnlUrl = null)
+        {
+            await this.HttpContext.SignOutAsync(IdentityConstants.ExternalScheme);
+
+            LoginViewModel model = new LoginViewModel()
+            {
+                ReturnUrl = returnlUrl
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var result =
+                await signInManager.PasswordSignInAsync(model.Username
+                , model.Password, model.RememberMe, false);
+
+            if (!result.Succeeded)
+            {
+                this.ModelState.AddModelError(string.Empty, "There was an error while logging you in! Please try again later or contact an administrator.");
+
+                return View(model);
+            }
+
+            return Redirect(model.ReturnUrl ?? "/Home/Index");
         }
     }
 }

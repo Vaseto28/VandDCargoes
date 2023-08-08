0c578cea3500c400422e10e35866fcb99119238f
vasilsmacbook <vaseto.k28@gmail.com>
2023-08-08 10:04:02 +0300

    Created new mapping table between Drivers and Cargoes tables

---
+++ VAndDCargoes/VAndDCargoes.Data.Models/DriversCargoes.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VAndDCargoes.Data.Models;
+
+public class DriversCargoes
+{
+	[Required]
+	public Guid DriverId { get; set; }
+
+	public virtual Driver Driver { get; set; } = null!;
+
+	[Required]
+	public Guid CargoId { get; set; }
+
+	public virtual Cargo Cargo { get; set; } = null!;
+}
+

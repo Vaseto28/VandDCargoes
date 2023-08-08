66080bf83374d5d2693a028958433e2b3de150e8
vasilsmacbook <vaseto.k28@gmail.com>
2023-08-08 10:04:21 +0300

    Created new mapping table between Drivers and Trailers

---
+++ VAndDCargoes/VAndDCargoes.Data.Models/DriversTrailers.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VAndDCargoes.Data.Models;
+
+public class DriversTrailers
+{
+	[Required]
+	public Guid DriverId { get; set; }
+
+	public virtual Driver Driver { get; set; } = null!;
+
+	[Required]
+	public Guid TrailerId { get; set; }
+
+	public virtual Trailer Trailer { get; set; } = null!;
+}
+

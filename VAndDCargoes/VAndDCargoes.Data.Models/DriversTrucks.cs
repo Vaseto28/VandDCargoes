5bd111d7e64c9f43e5c5659c7df370bbb2300b1c
vasilsmacbook <vaseto.k28@gmail.com>
2023-08-08 10:04:38 +0300

    Created new mapping table between Drivers and Trucks

---
+++ VAndDCargoes/VAndDCargoes.Data.Models/DriversTrucks.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VAndDCargoes.Data.Models;
+
+public class DriversTrucks
+{
+	[Required]
+	public Guid DriverId { get; set; }
+
+	public virtual Driver Driver { get; set; } = null!;
+
+	[Required]
+	public Guid TruckId { get; set; }
+
+	public virtual Truck Truck { get; set; } = null!;
+}
+

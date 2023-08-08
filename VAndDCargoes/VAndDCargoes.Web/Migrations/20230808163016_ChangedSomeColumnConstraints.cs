b0261e9c00c61c398c23bbc99db14b835c87f7b0
vasilsmacbook <vaseto.k28@gmail.com>
2023-08-08 19:44:27 +0300

    Updated Cargo's name max length from 15 to 30 symbols

---
+++ VAndDCargoes/VAndDCargoes.Web/Migrations/20230808163016_ChangedSomeColumnConstraints.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VAndDCargoes.Web.Migrations
+{
+    public partial class ChangedSomeColumnConstraints : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Cargoes",
+                type: "nvarchar(30)",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(15)",
+                oldMaxLength: 15);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Cargoes",
+                type: "nvarchar(15)",
+                maxLength: 15,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(30)",
+                oldMaxLength: 30);
+        }
+    }
+}

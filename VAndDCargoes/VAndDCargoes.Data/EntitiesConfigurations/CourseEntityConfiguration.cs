5ce3b94deac3fb24049a71f7a113cbef9067db28
vasilsmacbook <vaseto.k28@gmail.com>
2023-08-11 18:10:45 +0300

    Edited entity type configurations where was needed

---
+++ VAndDCargoes/VAndDCargoes.Data/EntitiesConfigurations/CourseEntityConfiguration.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using VAndDCargoes.Data.Models;
+
+namespace VAndDCargoes.Data.EntitiesConfigurations;
+
+public class CourseEntityConfiguration : IEntityTypeConfiguration<Course>
+{
+    public void Configure(EntityTypeBuilder<Course> builder)
+    {
+        builder
+            .HasOne(x => x.Driver)
+            .WithMany(x => x.Courses)
+            .HasForeignKey(x => x.DriverId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder
+            .HasOne(x => x.Truck)
+            .WithMany(x => x.Courses)
+            .HasForeignKey(x => x.TruckId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder
+            .HasOne(x => x.Trailer)
+            .WithMany(x => x.Courses)
+            .HasForeignKey(x => x.TrailerId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder
+            .HasOne(x => x.Cargo)
+            .WithMany(x => x.Courses)
+            .HasForeignKey(x => x.CargoId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder
+            .Property(x => x.Reward)
+            .HasColumnType("decimal(18,2)");
+    }
+}
+
---
+++ VAndDCargoes/VAndDCargoes.Data/EntitiesConfigurations/DriverEntityConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using VAndDCargoes.Data.Models;
+
+namespace VAndDCargoes.Data.EntitiesConfigurations;
+
+public class DriverEntityConfiguration : IEntityTypeConfiguration<Driver>
+{
+    public void Configure(EntityTypeBuilder<Driver> builder)
+    {
+        builder
+            .Property(x => x.Ballance)
+            .HasColumnType("decimal(18,2)")
+            .HasDefaultValue(0m);
+    }
+}
+

4aa0df81c2a904bcc519301c3509e8b3e4f59d4a
vasilsmacbook <vaseto.k28@gmail.com>
2023-08-12 15:20:10 +0300

    Created StartCourse view model

---
+++ VAndDCargoes/VAndDCargoes.Web.Models/Course/StartCourseViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel.DataAnnotations;
+using VAndDCargoes.Web.ViewModels.Cargo;
+using VAndDCargoes.Web.ViewModels.Trailer;
+using VAndDCargoes.Web.ViewModels.Truck;
+using static VAndDCargoes.Common.EntitiesValidations.Course;
+
+namespace VAndDCargoes.Web.ViewModels.Course;
+
+public class StartCourseViewModel
+{
+	[Required]
+	[Display(Name = "Departure city")]
+	[StringLength(DepartureCityMaxLength, MinimumLength = DepartureCityMinLength)]
+	public string DepartureCity { get; set; } = null!;
+
+	[Required]
+	[Display(Name = "Arrival city")]
+	[StringLength(ArrivalCityMaxLength, MinimumLength = ArrivalCityMinLength)]
+	public string ArrivalCity { get; set; } = null!;
+
+	[Range(DistanceMinValue, DistanceMaxValue)]
+	public int Distance { get; set; }
+
+	[Required]
+	public IEnumerable<TruckAllViewModel> Trucks { get; set; } = new HashSet<TruckAllViewModel>();
+
+	[Required]
+	[Display(Name = "Truck")]
+	public string TruckId { get; set; } = null!;
+
+	[Required]
+	public IEnumerable<TrailerAllViewModel> Trailers { get; set; } = new HashSet<TrailerAllViewModel>();
+
+    [Required]
+	[Display(Name = "Trailer")]
+	public string TrailerId { get; set; } = null!;
+
+	[Required]
+	public IEnumerable<CargoAllViewModel> Cargoes { get; set; } = new HashSet<CargoAllViewModel>();
+
+    [Required]
+	[Display(Name = "Cargo")]
+	public string CargoId { get; set; } = null!;
+}
+

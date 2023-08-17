6eb421037248d711d3d2fee6ca6a62e80da4c539
vasilsmacbook <vaseto.k28@gmail.com>
2023-08-17 09:54:25 +0300

    Added Repairment view models

---
+++ VAndDCargoes/VAndDCargoes.Web.Models/Repairment/AllRepairmentsViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static VAndDCargoes.Common.EntitiesValidations.Repairment;
+
+namespace VAndDCargoes.Web.ViewModels.Repairment;
+
+public class AllRepairmentsViewModel
+{
+	public int Id { get; set; }
+
+    [Required]
+    [Display(Name = "Repairment type")]
+    [StringLength(TypeMaxLength, MinimumLength = TypeMinLength)]
+    public string Type { get; set; } = null!;
+
+    [Required]
+    [StringLength(DescriptionMaxLenght, MinimumLength = DescriptionMinLength)]
+    public string Description { get; set; } = null!;
+
+    [Range(CostMinValue, CostMaxValue)]
+    public int Cost { get; set; }
+
+    [Required]
+    [Display(Name = "Truck")]
+    public string TruckId { get; set; } = null!;
+}
+
---
+++ VAndDCargoes/VAndDCargoes.Web.Models/Repairment/CreateRepairmentViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using VAndDCargoes.Web.ViewModels.Truck;
+using static VAndDCargoes.Common.EntitiesValidations.Repairment;
+
+namespace VAndDCargoes.Web.ViewModels.Repairment;
+
+public class CreateRepairmentViewModel
+{
+	public CreateRepairmentViewModel()
+	{
+		this.Trucks = new HashSet<TruckAllViewModel>();
+	}
+
+	[Required]
+	[Display(Name = "Repairment type")]
+	[StringLength(TypeMaxLength, MinimumLength = TypeMinLength)]
+	public string Type { get; set; } = null!;
+
+	[Required]
+	[StringLength(DescriptionMaxLenght, MinimumLength = DescriptionMinLength)]
+	public string Description { get; set; } = null!;
+
+	[Range(CostMinValue, CostMaxValue)]
+	public int Cost { get; set; }
+
+	[Required]
+    [Display(Name = "Truck")]
+    public Guid TruckId { get; set; }
+
+	public IEnumerable<TruckAllViewModel> Trucks { get; set; } = null!;
+}
+

71f17bba624b29eb87fa128ac62402c39e38072a
vasilsmacbook <vaseto.k28@gmail.com>
2023-08-17 19:09:38 +0300

    Updated some of the views and added new functionalities

---
+++ VAndDCargoes/VAndDCargoes.Web.Models/Repairment/CreateRepairmentOfTrailerViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using VAndDCargoes.Web.ViewModels.Trailer;
+using static VAndDCargoes.Common.EntitiesValidations.Repairment;
+
+namespace VAndDCargoes.Web.ViewModels.Repairment;
+
+public class CreateRepairmentOfTrailerViewModel
+{
+    public CreateRepairmentOfTrailerViewModel()
+    {
+        this.Trailers = new HashSet<TrailerAllViewModel>();
+    }
+
+    [Required]
+    [Display(Name = "Repairment type")]
+    public int Type { get; set; }
+
+    [Required]
+    [StringLength(DescriptionMaxLenght, MinimumLength = DescriptionMinLength)]
+    public string Description { get; set; } = null!;
+
+    public int Quantity { get; set; }
+
+    public int Cost { get; set; }
+
+    [Required]
+    [Display(Name = "Trailer")]
+    public Guid TrailerId { get; set; }
+
+    public IEnumerable<TrailerAllViewModel> Trailers { get; set; } = null!;
+}
+
